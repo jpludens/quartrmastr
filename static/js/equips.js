ef1bc80fe3a5d5d18857d0520337ee268ed7d407
jpalm <palm.jay@gmail.com>
2016-11-24 10:52:43 -0500

    Added level comparison view.

--- static/js/equips.js
+++ static/js/equips.js
@@ -11,8 +11,9 @@ var request = new Request(
 );
 fetch(request).then(function(response) {
   response.json().then(function(json) {
+    let equips = json.map(e => new Equip(e));
     ReactDOM.render(
-      <EquipSummariesContainer equips={json} />,
+      <EquipSummariesContainer equips={equips} />,
       document.getElementById('summaries')
     );
     ReactDOM.render(
@@ -22,6 +23,24 @@ fetch(request).then(function(response) {
   });
 });
 
+class Equip {
+  constructor(equipData) {
+    this.name = equipData.equipName;
+    this.slot = equipData.equipSlot;
+    this.levelsData = equipData.levels;
+    this.maxLevel = equipData.levels[equipData.levels.length-1].level
+  }
+
+  getStatsAtLevel(level) {
+    return this.levelsData[level - 1].stats;
+  }
+
+  getUpgradeMaterialsAtLevel(level) {
+    if (level < this.maxLevel) {
+      return this.levelsData[level - 1].upgradeMaterials;
+    }
+  }
+}
 
 // Equips Summary:
 // A list of each Equip. When clicked, its details are displayed.
@@ -35,13 +54,13 @@ const renderEquipSummaries = equips => (
 
 class EquipSummary extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   render () {
     return (
       <li onClick={this.handleClick.bind(this)}>
-        {this.props.equip.equipName} ({this.props.equip.equipSlot})
+        {this.props.equip.name} ({this.props.equip.slot})
       </li>
     );
   }
@@ -55,91 +74,217 @@ class EquipSummary extends React.Component {
 }
 
 
-// Equip Details:
-// Show the stats and upgrade materials required at the current level.
-// Include an indicator of the current level and buttons to change it.
+// Equip Details
 class EquipDetails extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      viewingLevel: 1
+      currentLevel: 1,
+      compareLevel: 2
     }
   }
 
   render() {
-    let equipLevel = this.props.equip.levels[this.state.viewingLevel-1];
+    let compareLevelData = null;
+    let statComparison = <p>This Equip is now beyond compare!</p>;
+    let upgradeRequirements = <p>It cannae even be upgraded!</p>;
+
+    let currentLevelData =
+      <div>
+        <h4>Current Level</h4>
+        <EquipLevelSelector
+          min={1}
+          max={this.props.equip.maxLevel}
+          setter={this.setCurrentLevel.bind(this)} />
+        <EquipLevelData
+          level={this.state.currentLevel}
+          stats={this.props.equip.getStatsAtLevel(this.state.currentLevel)} />
+      </div>;
+
+    if (this.state.compareLevel) {
+      compareLevelData =
+        <div>
+          <h4>Compared to Level</h4>
+          <EquipLevelSelector
+            min={this.state.currentLevel + 1}
+            max={this.props.equip.maxLevel}
+            setter={this.setCompareLevel.bind(this)} />
+          <EquipLevelData
+            level={this.state.compareLevel}
+            stats={this.props.equip.getStatsAtLevel(this.state.compareLevel)} />
+        </div>;
+
+      let currentStats = this.props.equip.getStatsAtLevel(this.state.currentLevel);
+      let compareStats = this.props.equip.getStatsAtLevel(this.state.compareLevel);
+      statComparison =
+        <div>
+          <h4>Stats Difference</h4>
+          {renderEquipStats(diffStats(compareStats, currentStats))}
+        </div>;
+
+      let upgradeMaterialsArray = [];
+      for (let l = this.state.currentLevel; l < this.state.compareLevel; l++) {
+        upgradeMaterialsArray.push(this.props.equip.getUpgradeMaterialsAtLevel(l));
+      }
+      upgradeRequirements =
+        <div>
+          <h4>Total Upgrade Materials</h4>
+          {renderUpgradeMaterials(sumUpgradeMaterials(upgradeMaterialsArray))}
+        </div>;
+    }
+
     return (
       <div>
-        <h3>Details for {this.props.equip.equipName}</h3>
-        {renderViewingLevelSelector(this)}
-        {renderStatsForEquipLevel(equipLevel)}
-        {renderUpgradeMaterialsForEquipLevel(equipLevel)}
+        <h3>Details for {this.props.equip.name}</h3>
+        <p>{this.props.equip.slot}</p>
+        <div id="details">
+          {currentLevelData}
+          <div>
+            {statComparison}
+            {upgradeRequirements}
+          </div>
+          {compareLevelData}
+        </div>
       </div>
     );
   }
 
-  setViewingLevel(viewingLevel) {
-    this.setState({viewingLevel: viewingLevel})
+  setCurrentLevel(newLevel) {
+    // Set the new current level.
+    this.setState({currentLevel: newLevel})
+    // If the new level is at max, we can no longer compare.
+    if (newLevel == this.props.equip.maxLevel) {
+      this.setState({compareLevel: null});
+    }
+    else if ((this.state.compareLevel || 0) <= newLevel) {
+      this.setState({compareLevel: newLevel + 1});
+    }
+  }
+
+  setCompareLevel(compareLevel) {
+    this.setState({compareLevel: compareLevel})
   }
 }
-
-// Level indicator / selector
-const renderViewingLevelSelector = equip => (
-  <div>
-    {renderViewingLevelButtons(equip)}
-    <p>Viewing data for level: {equip.state.viewingLevel}</p>
-  </div>
-
-);
-
-const renderViewingLevelButtons = equip => (
-  <div>
-    {[...Array(5).keys()].map(n => renderViewingLevelButton(equip, ++n))}
-  </div>
-);
-
-const renderViewingLevelButton = function(parent, level) {
-  return <button onClick={parent.setViewingLevel.bind(parent, level)}>{level}</button>
+EquipDetails.propTypes = {
+  equip: React.PropTypes.object.isRequired
 }
+
+
+// Display a set of buttons to select a particular level of an equip.
+class EquipLevelSelector extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div>
+        {[...Array(this.props.max - this.props.min + 1).keys()].map(
+          n =>
+            <button onClick={() => this.props.setter(n + this.props.min)}>
+              {n + this.props.min}
+            </button>)}
+      </div>
+    );
+  }
+}
+EquipLevelSelector.propTypes = {
+  setter: React.PropTypes.func.isRequired,
+  min: React.PropTypes.number.isRequired,
+  max: React.PropTypes.number.isRequired
+}
+
+
+// Display the stats, (effects), (resistances), and upgrade requirements
+// for a specific level of an equip.
+class EquipLevelData extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let matsContent = null;
+    if (this.props.mats) {
+      matsContent =
+        <div>
+          <h4>Upgrade to Level {this.props.level+1}</h4>
+          {renderUpgradeMaterials(this.props.mats)}
+        </div>;
+    }
+
+    return (
+      <div>
+        <div>
+          <h4>Stats at Level {this.props.level}</h4>
+          {renderEquipStats(this.props.stats)}
+        </div>
+        {matsContent}
+      </div>
+    );
+  }
+}
+EquipLevelData.propTypes = {
+  level: React.PropTypes.number.isRequired,
+  stats: React.PropTypes.object.isRequired,
+  mats: React.PropTypes.object
+}
+
+
 
 // Stats
-const renderStatsForEquipLevel = equipLevel => (
-  <div>
-    <h4>Stats</h4>
-    <ul>
-      <li>Health Points: {equipLevel.stats.healthPoints}</li>
-      <li>Magic Points: {equipLevel.stats.magicPoints}</li>
-      <li>Physical Attack: {equipLevel.stats.physicalAttack}</li>
-      <li>Magic Attack: {equipLevel.stats.magicAttack}</li>
-      <li>Physical Defence: {equipLevel.stats.physicalDefence}</li>
-      <li>Magic Defence: {equipLevel.stats.magicDefence}</li>
-      <li>Accuracy: {equipLevel.stats.accuracy}</li>
-      <li>Evade: {equipLevel.stats.evade}</li>
-    </ul>
-  </div>
+const renderEquipStats = stats => (
+  <ul>
+    <li>Health Points: {stats.healthPoints}</li>
+    <li>Magic Points: {stats.magicPoints}</li>
+    <li>Physical Attack: {stats.physicalAttack}</li>
+    <li>Magic Attack: {stats.magicAttack}</li>
+    <li>Physical Defence: {stats.physicalDefence}</li>
+    <li>Magic Defence: {stats.magicDefence}</li>
+    <li>Accuracy: {stats.accuracy}</li>
+    <li>Evade: {stats.evade}</li>
+  </ul>
 );
 
-// Upgrade materials
-const renderUpgradeMaterialsForEquipLevel = function(equipLevel) {
-  // Return a list of upgrade materials, but max levels lack that property.
-  if (equipLevel.hasOwnProperty("upgradeMaterials")) {
-    return (
-      <div>
-        <h4>Upgrade Materials</h4>
-        <ul>
-          {equipLevel.upgradeMaterials.map(um => renderUpgradeMaterial(um))}
-        </ul>
-      </div>
-    );
+const diffStats = function (statsA, statsB) {
+  let diff = {}
+  for (let stat in statsA) {
+    if (statsA.hasOwnProperty(stat)) {
+      diff[stat] = statsA[stat] - statsB[stat];
+    }
   }
-  else {
-    return <p>Cannae be impryuved!</p> 
-  }  
-};
+  return diff;
+}
+
+
+// Upgrade Materials
+const renderUpgradeMaterials = upgradeMaterials => (
+  <ul>
+    {upgradeMaterials.map(um => renderUpgradeMaterial(um))}
+  </ul>
+);
 
 const renderUpgradeMaterial = upgradeMaterial => (
   <li>
     {upgradeMaterial.materialName}: {upgradeMaterial.materialAmount}
   </li>
 );
+
+function sumUpgradeMaterials(upgradeMaterialsArray) {
+  let totals = new Map();
+  [...upgradeMaterialsArray].forEach(function(upgradeMaterials) {
+    [...upgradeMaterials].forEach(function(upgradeMaterial) {
+      // If this is a material that hasn't been seen, set a count for it to 0
+      let name = upgradeMaterial.materialName;
+      if (!totals.has(name)) {
+        totals.set(name, {
+            materialName: name,
+            materialAmount: 0
+        });
+      }
+      // Now add the amount for this material from this set
+      totals.get(name).materialAmount += upgradeMaterial.materialAmount;
+    });
+  });
+  return [...totals.values()];
+};
